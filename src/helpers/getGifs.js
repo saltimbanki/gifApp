5ef5369f1834eef072b6a737286547c5ac2b90cd
Jorge <jorgebabarro@hotmail.com>
2023-09-21 16:26:57 +0200

    cambiamos de diseño

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async (categoria) => {
-  const url = `https://api.giphy.com/v1/gifs/search?api_key=A23fV2UXIXBsfk98nEVLNaWHvyZ062gT&q=${categoria}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=A23fV2UXIXBsfk98nEVLNaWHvyZ062gT&q=${categoria}&limit=10&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
   const resp = await fetch(url);
   const { data } = await resp.json();
 
@@ -9,5 +9,5 @@ export const getGifs = async (categoria) => {
     url: img.images.fixed_height_downsampled.url,
   }));
   console.log(gifs);
-  return gifs;
+  return [...gifs,...gifs];
 };
